232104f8e064550dc7f9bccd71f5d7b89cb7de18
Sebastián García Martínez <sebastiangarcia_m@outlook.com>
2022-02-21 16:36:27 +0100

    commit first attempt to solution

--- utils/card.py
+++ utils/card.py
@@ -1,1 +1,26 @@
+class Symbol:
 
+    icon_options = ['♥',' ♦', '♣', '♠']
+
+    def __init__(self, color, icon):
+
+        self.color = color
+        self.icon = icon
+
+    def __str__ (self):
+        return f"{self.color} - {self.icon}"
+
+class Card(Symbol):
+
+    value_options = ['A','2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
+    def __init__(self, color, icon, value):
+        Symbol.__init__(self, color, icon)
+        self.value = value
+    
+    def __str__ (self):
+        return f"{self.color} - {self.icon} - {self.value}"
+
+
+myCard = Card("red",'♥', '10')
+print(myCard)
